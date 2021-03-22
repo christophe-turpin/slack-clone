7479245a8a1b9129af58bcc31e917dd4e06ed58c
Christophe TURPIN <christophe-turpin@github.com>
2021-03-22 14:03:26 +0100

    persistent user + logout ok

--- src/App.js
+++ src/App.js
@@ -5,17 +5,44 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./components/Chat/Chat";
 import Login from "./components/Login/Login";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
+import { actionTypes } from "./reducer";
+import { useEffect, useState } from "react";
 
 function App() {
   // eslint-disable-next-line no-unused-vars
   const [{ user }, dispatch] = useStateValue();
+  const [initializing, setInitializing] = useState(true);
 
-  console.log(user);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: user,
+        });
+      } else {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: null,
+        });
+      }
+      if (initializing) {
+        setInitializing(false);
+      }
+    });
+
+    return unsubscribe;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="app">
       <Router>
         {!user ? (
-          <Login />
+          <Route path="/">
+            <Login />
+          </Route>
         ) : (
           <>
             <Header />
@@ -29,7 +56,6 @@ function App() {
                   <h1>Welcome</h1>
                 </Route>
               </Switch>
-              {/* React Router -> Chat screen*/}
             </div>
           </>
         )}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,23 @@ import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import Logout from "@material-ui/icons/ExitToApp";
 import { useStateValue } from "../../StateProvider";
+import { auth } from "../../firebase";
+import { actionTypes } from "../../reducer";
+import { useHistory } from "react-router";
 
 function Header() {
-  const [{ user }] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
+  const history = useHistory();
+  const signOut = () => {
+    auth.signOut();
+    dispatch({
+      type: actionTypes.SET_USER,
+      user: null,
+    });
+    history.push("/");
+  };
   return (
     <div className="header">
       <div className="header__left">
@@ -16,6 +29,7 @@ function Header() {
           alt={user?.displayName}
           src={user?.photoURL}
         />
+        <Logout onClick={signOut} />
         <AccessTimeIcon />
       </div>
       <div className="header__search">
